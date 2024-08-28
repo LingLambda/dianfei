7aa08da1e739ce6b0a276aa75f2d12e7fd30c6ec
ling <abc1514671906@163.com>
2024-08-28 15:20:48 +0800

    更新使用无头模式

--- src/Service/DianFeiService.ts
+++ src/Service/DianFeiService.ts
@@ -6,25 +6,20 @@ function sleep(ms: number): Promise<void> {
 
 
 export class DianFeiService{
-   async getDianFeiImg(){
-        // 启动浏览器
-        const browser = await puppeteer.launch({ headless: false }); // 设为 false 以查看操作
+   async getDianFeiImg(user:string,pwd:string){
+        const browser = await puppeteer.launch({args:['--no-sandbox', '--disable-setuid-sandbox'], headless: true }); // 设为 false 会导致无图形界面的bug
         const page: Page = await browser.newPage();
     
-        // 访问登录页面
         await page.goto('http://pay.czrxdzonline.cn/wxms/', { waitUntil: 'networkidle0' });
     
-        // 输入手机号
         await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input');
-        await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input', '15587875682');
+        await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input', user);
     
-        // 输入密码
         await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input');
-        await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input', '1514671906');
+        await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input', pwd);
     
         await sleep(1000);
     
-        // 点击登录按钮
         await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-button');
         await page.click('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-button');
     
@@ -32,10 +27,8 @@ export class DianFeiService{
     
         await sleep(8000);
     
-        // 截图
         const screenshotData=await page.screenshot({ fullPage: true });
         
-        // 关闭浏览器
         await browser.close();
         return screenshotData
     }
--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,24 @@ import { DianFeiService } from './Service/DianFeiService';
 export const name = 'dianfei'
 export const inject = {
 }
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
-
-const dfs:DianFeiService=new DianFeiService();
-export function apply(ctx: Context) {
-  // write your plugin here
-  ctx.command('dianfei', '电费')
-  .action((argv) => getDianFeiImg())
+export interface Config {
+  user:string,
+  pwd:string
 }
 
-async function getDianFeiImg(){
-  return h.image(await dfs.getDianFeiImg(), 'image/png')
+export const Config: Schema<Config> = Schema.object({
+  user: Schema.string().required().description('用户名'),
+  pwd: Schema.string().required().description('密码')
+})
+
+const dfs:DianFeiService=new DianFeiService();
+export function apply(ctx: Context,config:Config) {
+  // write your plugin here
+  ctx.command('dianfei', '电费')
+  .action((argv) => getDianFeiImg(config))
+}
+
+async function getDianFeiImg(config: Config){
+
+  return h.image(await dfs.getDianFeiImg(config.user,config.pwd), 'image/png')
 }
