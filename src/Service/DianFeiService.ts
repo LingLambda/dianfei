091d76b585a56846ee2b9781b463bcbb934de168
ling <abc1514671906@163.com>
2024-09-10 15:55:38 +0800

    变成一个正常的项目

--- src/Service/DianFeiService.ts
+++ src/Service/DianFeiService.ts
@@ -1,35 +1,42 @@
-import puppeteer, { Page } from 'puppeteer';
-
+import { Context } from 'koishi';
+import 'koishi-plugin-puppeteer'
 function sleep(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 
-export class DianFeiService{
-   async getDianFeiImg(user:string,pwd:string){
-        const browser = await puppeteer.launch({args:['--no-sandbox', '--disable-setuid-sandbox'], headless: true }); // 设为 false 会导致无图形界面的bug
-        const page: Page = await browser.newPage();
-    
+export class DianFeiService {
+    async getDianFeiImg(ctx: Context, url: string, user: string, pwd: string) {
+        let page = await ctx.puppeteer.browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36');
+        await page.goto(url, {
+            waitUntil: 'networkidle2'  // 等待加载完毕
+        });
+        ctx.logger('dianfei').info('加载登录完成');
         await page.goto('http://pay.czrxdzonline.cn/wxms/', { waitUntil: 'networkidle0' });
-    
-        await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input');
-        await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input', user);
-    
-        await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input');
-        await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input', pwd);
-    
-        await sleep(1000);
-    
-        await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-button');
-        await page.click('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-button');
-    
-        await page.waitForNavigation();
-    
-        await sleep(8000);
-    
-        const screenshotData=await page.screenshot({ fullPage: true });
-        
-        await browser.close();
-        return screenshotData
+        try {
+            await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input', { timeout: 2000 }); // 设置 5 秒超时
+            ctx.logger('dianfei').info('元素存在');
+            await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input');
+            await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(2) > uni-view > uni-input > div > input', user);
+
+            await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input');
+            await page.type('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-view:nth-child(3) > uni-view > uni-input > div > input', pwd);
+
+            await sleep(1500);
+
+            await page.waitForSelector('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-button');
+            await page.click('body > uni-app > uni-page > uni-page-wrapper > uni-page-body > uni-view > uni-button');
+            ctx.logger('dianfei').info('登录完成');
+            await page.waitForNavigation();
+            await sleep(8000);
+        } catch (error) {
+            ctx.logger('dianfei').info('元素不存在');
+            await sleep(6000);
+        }
+        const screenshotData = await page.screenshot({ fullPage: true });
+        ctx.logger('dianfei').info('截图完成');
+        await page.close();
+        return screenshotData;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import {  Context, h, Schema } from 'koishi'
+import {  Argv, Context, h, Schema } from 'koishi'
 import { DianFeiService } from './Service/DianFeiService';
 
 export const name = 'dianfei'
 export const inject = {
+  required: ['puppeteer']
 }
 export interface Config {
+  url:string,
   user:string,
   pwd:string
 }
 
 export const Config: Schema<Config> = Schema.object({
+  url: Schema.string().description(''),
   user: Schema.string().required().description('用户名'),
   pwd: Schema.string().required().description('密码')
 })
@@ -18,10 +21,10 @@ const dfs:DianFeiService=new DianFeiService();
 export function apply(ctx: Context,config:Config) {
   // write your plugin here
   ctx.command('dianfei', '电费')
-  .action((argv) => getDianFeiImg(config))
+  .action((_) => getDianFeiImg(_,ctx,config))
 }
 
-async function getDianFeiImg(config: Config){
-
-  return h.image(await dfs.getDianFeiImg(config.user,config.pwd), 'image/png')
+async function getDianFeiImg(argv:Argv,ctx: Context,config: Config){
+  argv.session.send('我去查了,稍等哦');
+  return h.image(await dfs.getDianFeiImg(ctx,config.url,config.user,config.pwd), 'image/png')
 }
